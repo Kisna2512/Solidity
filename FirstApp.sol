9e4765b8e6d803d3d49b04d817d3457fd94dd28c
Krishna Kotgire <krishnakotgire12369@gmail.com>
2023-12-21 22:26:53 +0530

    NFT Counter Contract

---
+++ FirstApp.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract NFTCount{
+    uint256 public numberOfNFT;
+
+    // it shows the no of nft's
+    function checkTotalNFT() public view returns(uint256)
+    {
+        return numberOfNFT;
+    }
+    // increment the nft
+    function addNFT() public {
+        numberOfNFT += 1;
+    }
+     // it decrement nft byb 1
+    function deleteNFT() public {
+        numberOfNFT -= 1;
+    }
+}
