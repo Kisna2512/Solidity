ab5f9cb54cadeab817874789794af9866675108a
Krishna Kotgire <krishnakotgire12369@gmail.com>
2024-01-08 23:15:55 +0530

    Do while in solidity

---
+++ do_while.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.13;
+
+contract dowhile {
+    uint256 [] data;
+    uint8 j=0;
+    function loop() public returns(uint256[] memory) {
+        do{
+            j++;
+            data.push(j);
+        }while(j < 5);
+        return data;
+    }
+}
